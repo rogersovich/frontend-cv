9d8ca4f3a7bec5214e8bb0986495cfaf29be8c0c
rogersovich <dimasroger89@gmail.com>
2020-12-01 13:01:40 +0700

    add search

--- components/AdminContactFormTable.vue
+++ components/AdminContactFormTable.vue
@@ -1,49 +1,65 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="$store.state.contact.contacts"
-    :loading="$store.state.loading"
-    loading-text="Tunggu sebentar gan !"
-    :items-per-page="10"
-    class="elevation-1"
-    mobile-breakpoint
-  >
-    <template v-slot:item="row">
-      <tr>
-        <td>{{ row.index + 1 }}</td>
-        <td>{{ row.item.email }}</td>
-        <td>{{ row.item.number_phone }}</td>
-        <td>{{ row.item.link_cv }}</td>
-        <td>
-          <v-btn
-            fab
-            x-small
-            color="primary"
-            :to="{
-              name: 'admin-contact-id',
-              params: { id: row.item.id },
-            }"
-            class="m-2 focus:tw-outline-none"
-          >
-            <v-icon>mdi-pencil</v-icon>
-          </v-btn>
-          <v-btn
-            fab
-            x-small
-            color="error"
-            class="m-2 focus:tw-outline-none"
-            @click="confirm(row.item)"
-          >
-            <v-icon>mdi-delete</v-icon>
-          </v-btn>
-        </td>
-      </tr>
-    </template>
-  </v-data-table>
+  <div>
+    <v-text-field
+      v-model="search"
+      :disabled="loading"
+      label="Search data here"
+      append-outer-icon="mdi-refresh"
+      @keypress.enter="searchData()"
+      @click:append-outer="getDataFromApi()"
+    ></v-text-field>
+    <v-data-table
+      :headers="headers"
+      :footer-props="footer"
+      :server-items-length="totalItems"
+      :items="items"
+      :disable-sort="true"
+      :loading="$store.state.loading"
+      loading-text="Tunggu sebentar gan !"
+      class="elevation-1"
+      mobile-breakpoint
+      @update:page="getDataFromApi"
+    >
+      <template v-slot:item="row">
+        <tr>
+          <td>{{ row.index + 1 }}</td>
+          <td>{{ row.item.email }}</td>
+          <td>{{ row.item.number_phone }}</td>
+          <td>{{ row.item.link_cv }}</td>
+          <td>
+            <v-btn
+              fab
+              x-small
+              color="primary"
+              :to="{
+                name: 'admin-contact-id',
+                params: { id: row.item.id },
+              }"
+              class="m-2 focus:tw-outline-none"
+            >
+              <v-icon>mdi-pencil</v-icon>
+            </v-btn>
+            <v-btn
+              fab
+              x-small
+              color="error"
+              class="m-2 focus:tw-outline-none"
+              @click="confirm(row.item)"
+            >
+              <v-icon>mdi-delete</v-icon>
+            </v-btn>
+          </td>
+        </tr>
+      </template>
+    </v-data-table>
+  </div>
 </template>
 
 <script>
+import serverSideTable from '~/mixins/serverSideTable'
+
 export default {
+  mixins: [serverSideTable],
   data() {
     return {
       headers: [
@@ -53,6 +69,7 @@ export default {
         { text: 'Link' },
         { text: 'Action', align: 'center', sortable: false, width: 100 },
       ],
+      url: 'contacts',
     }
   },
   methods: {
---
+++ mixins/serverSideTable.js
@@ -0,0 +1,49 @@
+const serverSideTable = {
+  data() {
+    return {
+      items: [],
+      totalItems: 0,
+      from: 0,
+      loading: false,
+      footer: {
+        disableItemsPerPage: true,
+      },
+      search: '',
+    }
+  },
+  created() {
+    this.getDataFromApi()
+  },
+  methods: {
+    async getDataFromApi(pagination) {
+      this.items = []
+      this.search = ''
+      this.loading = true
+      const { data } = await this.$axios.get(`/${this.url}?page=` + pagination)
+      this.loading = false
+      this.items = data.data
+      this.totalItems = data.total
+      this.from = data.from
+    },
+    async searchData() {
+      if (this.search === '') {
+        const { data } = await this.$axios.get(`/${this.url}?page=1`)
+        this.loading = false
+        this.items = data.data
+        this.totalItems = data.total
+        this.from = data.from
+      } else {
+        this.items = []
+        this.loading = true
+        const { data } = await this.$axios.get(
+          `/${this.url}?search=` + this.search
+        )
+        this.totalItems = data.length
+        this.loading = false
+        this.items = data
+      }
+    },
+  },
+}
+
+export default serverSideTable
